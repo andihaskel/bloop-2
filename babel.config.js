67acb6f90bcbf87ea2fc22f4455a88c40fea7b77
Andrés Haskel <andres.haskel@gmail.com>
2025-05-15 18:25:10 -0300

    Fix React Native runtime error

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,9 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // Optimize reanimated
+      // Required for expo-router
+      'expo-router/babel',
+      // Reanimated plugin must be listed last
       'react-native-reanimated/plugin',
     ],
   };
